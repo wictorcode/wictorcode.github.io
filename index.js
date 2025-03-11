c8aa849bead222eea7a08908b54b391ee7974032
wictorcode <wictor.dev.pro@gmail.com>
2025-03-11 19:03:32 +0100

    About page

--- index.js
+++ index.js
@@ -3,7 +3,18 @@
 //===================//
 
 const body = document.querySelector("body");
-const button_competences = document.getElementById("nav-button-3");
+// Buttons
+const navButton_aPropos = document.getElementById("nav-button-1");
+const navButton_projets = document.getElementById("nav-button-2");
+const navButton_competences = document.getElementById("nav-button-3");
+const navButton_blog = document.getElementById("nav-button-4");
+
+//=============//
+// OTHER STUFF //
+//=============//
+
+let pageCache_aPropos = null;
+let pageCache_competences = null;
 
 //======================//
 // FUNCTION DEFINITIONS //
@@ -16,17 +27,54 @@ function initBackgroundMovement() {
     })
 }
 
+// A PROPOS  --  FETCHING AND CACHING
+navButton_aPropos.addEventListener("click", () => {
 
-button_competences.addEventListener("click", () => {
-    fetch("content/competences.html").then(response => {
-        if(response.ok) {
-            return response.text()
-        }
-        console.log("Failed to retrieve data");
-    }).then(html => {
-        document.getElementById("right-main-container").innerHTML = html
-    })
+    let container = document.getElementById("right-main-container")
+
+    if (pageCache_aPropos !== null) {
+        container.innerHTML = pageCache_aPropos
+    } else {
+        fetch("content/a-propos.html").then(response => {
+
+            console.log("Loading...");
+    
+            if(response.ok) {
+                return response.text()
+            }
+            console.log("Failed to retrieve data");
+        }).then(html => {
+            pageCache_aPropos = html
+            container.innerHTML = html
+        })
+    }
 })
 
+// COMPETENCES  --  FETCHING AND CACHING
+navButton_competences.addEventListener("click", () => {
+
+    let container = document.getElementById("right-main-container")
+
+    if (pageCache_competences !== null) {
+        container.innerHTML = pageCache_competences
+    } else {
+        fetch("content/competences.html").then(response => {
+
+            console.log("Loading...");
+    
+            if(response.ok) {
+                return response.text()
+            }
+            console.log("Failed to retrieve data");
+        }).then(html => {
+            pageCache_competences = html
+            container.innerHTML = html
+        })
+    }
+})
+
+//======//
+// INIT //
+//======//
 
 initBackgroundMovement()

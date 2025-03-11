6c02abc67b1d39743eaa625351339f7232fb0159
wictorcode <wictor.dev.pro@gmail.com>
2025-03-11 19:27:17 +0100

    Project page

--- index.js
+++ index.js
@@ -4,17 +4,20 @@
 
 const body = document.querySelector("body");
 // Buttons
-const navButton_aPropos = document.getElementById("nav-button-1");
-const navButton_projets = document.getElementById("nav-button-2");
+const navButton_aPropos     = document.getElementById("nav-button-1");
+const navButton_projets     = document.getElementById("nav-button-2");
 const navButton_competences = document.getElementById("nav-button-3");
-const navButton_blog = document.getElementById("nav-button-4");
+const navButton_blog        = document.getElementById("nav-button-4");
+const contentContainer      = document.getElementById("right-main-container")
 
 //=============//
 // OTHER STUFF //
 //=============//
 
-let pageCache_aPropos = null;
+let pageCache_aPropos     = null;
+let pageCache_projets     = null;
 let pageCache_competences = null;
+let pageCache_blog        = null;
 
 //======================//
 // FUNCTION DEFINITIONS //
@@ -27,13 +30,15 @@ function initBackgroundMovement() {
     })
 }
 
+function route() {
+    
+}
+
 // A PROPOS  --  FETCHING AND CACHING
 navButton_aPropos.addEventListener("click", () => {
 
-    let container = document.getElementById("right-main-container")
-
     if (pageCache_aPropos !== null) {
-        container.innerHTML = pageCache_aPropos
+        contentContainer.innerHTML = pageCache_aPropos
     } else {
         fetch("content/a-propos.html").then(response => {
 
@@ -45,7 +50,7 @@ navButton_aPropos.addEventListener("click", () => {
             console.log("Failed to retrieve data");
         }).then(html => {
             pageCache_aPropos = html
-            container.innerHTML = html
+            contentContainer.innerHTML = html
         })
     }
 })
@@ -53,10 +58,8 @@ navButton_aPropos.addEventListener("click", () => {
 // COMPETENCES  --  FETCHING AND CACHING
 navButton_competences.addEventListener("click", () => {
 
-    let container = document.getElementById("right-main-container")
-
     if (pageCache_competences !== null) {
-        container.innerHTML = pageCache_competences
+        contentContainer.innerHTML = pageCache_competences
     } else {
         fetch("content/competences.html").then(response => {
 
@@ -68,7 +71,49 @@ navButton_competences.addEventListener("click", () => {
             console.log("Failed to retrieve data");
         }).then(html => {
             pageCache_competences = html
-            container.innerHTML = html
+            contentContainer.innerHTML = html
+        })
+    }
+})
+
+// PROJETS  --  FETCHING AND CACHING
+navButton_projets.addEventListener("click", () => {
+
+    if (pageCache_projets !== null) {
+        contentContainer.innerHTML = pageCache_projets
+    } else {
+        fetch("content/projets.html").then(response => {
+
+            console.log("Loading...");
+    
+            if(response.ok) {
+                return response.text()
+            }
+            console.log("Failed to retrieve data");
+        }).then(html => {
+            pageCache_projets = html
+            contentContainer.innerHTML = html
+        })
+    }
+})
+
+// BLOG  --  FETCHING AND CACHING
+navButton_blog.addEventListener("click", () => {
+
+    if (pageCache_blog !== null) {
+        contentContainer.innerHTML = pageCache_blog
+    } else {
+        fetch("content/blog.html").then(response => {
+
+            console.log("Loading...");
+    
+            if(response.ok) {
+                return response.text()
+            }
+            console.log("Failed to retrieve data");
+        }).then(html => {
+            pageCache_blog = html
+            contentContainer.innerHTML = html
         })
     }
 })

55cceaa4f475b25ba3084254005df64e155b8821
wictorcode <wictor.dev.pro@gmail.com>
2025-08-16 13:48:01 +0200

    new version

---
+++ data.js
@@ -0,0 +1,12 @@
+export const skills = [
+    {name: "Typescript", xp: 3},
+    {name: "Javascript", xp: 3},
+    {name: "Angular", xp: 3},
+    {name: "SQL", xp: 3},
+    {name: "Lua", xp: 3},
+    {name: "Python", xp: 3},
+    {name: "PHP", xp: 2},
+    {name: "Symfony", xp: 1},
+    
+    
+]
